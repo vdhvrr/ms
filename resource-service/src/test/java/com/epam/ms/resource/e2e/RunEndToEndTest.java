a7d9815238e63c056fb07aae2d7b15c529072f0c
vdhvrr <Valeryia_Mazheika@epam.com>
2022-05-04 01:06:29 +0400

    Module 3 - e2e

---
+++ resource-service/src/test/java/com/epam/ms/resource/e2e/RunEndToEndTest.java
@@ -0,0 +1,9 @@
+package com.epam.ms.resource.e2e;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/feature")
+public class RunEndToEndTest {}
